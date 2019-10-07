9c51e12de8c68f169cd9cff45bf5305d0c0792db
Nikolay Grozdanov <ngrozdanov@magicleap.com>
2019-10-07 16:14:45 -0400

    Resolve missing references and DropdownListItem verification

--- src/platform/lumin-runtime/elements/builders/dropdown-list-builder.js
+++ src/platform/lumin-runtime/elements/builders/dropdown-list-builder.js
@@ -2,11 +2,15 @@
 
 import { ui } from 'lumin';
 
-import { TextContainerBuilder } from './text-container-builder.js';
+import { UiNodeBuilder } from './ui-node-builder.js';
+import { PrimitiveTypeProperty } from '../properties/primitive-type-property.js';
+import { ArrayProperty } from '../properties/array-property.js';
+import { ClassProperty } from '../properties/class-property.js';
+import { PropertyDescriptor } from '../properties/property-descriptor.js';
 
-export class DropdownListBuilder extends TextContainerBuilder {
-    constructor() {
-        super();
+export class DropdownListBuilder extends UiNodeBuilder {
+  constructor () {
+    super();
 
     // ButtonModel
     // List
@@ -23,42 +27,53 @@ export class DropdownListBuilder extends TextContainerBuilder {
     // ];
     // this._propertyDescriptors['listFont'] = new ClassProperty('listFont', 'setListFont', false, listFontProperties);
 
-        this._propertyDescriptors['iconSize'] = new PrimitiveTypeProperty('iconSize', 'setIconSize', true, 'number');
-        this._propertyDescriptors['iconColor'] = new ArrayProperty('iconColor', 'setIconColor', true, 'vec4');
-        this._propertyDescriptors['listMaxHeight'] = new PrimitiveTypeProperty('listMaxHeight', 'setListMaxHeight', true, 'number');
-        this._propertyDescriptors['listTextSize'] = new PrimitiveTypeProperty('listTextSize', 'setListTextSize', true, 'number');
-        this._propertyDescriptors['maxCharacterLimit'] = new PrimitiveTypeProperty('maxCharacterLimit', 'setMaxCharacterLimit', true, 'number');
-        this._propertyDescriptors['multiSelect'] = new PrimitiveTypeProperty('multiSelect', 'setMultiSelect', true, 'boolean');
-        this._propertyDescriptors['showList'] = new PrimitiveTypeProperty('showList', 'showList', true, 'boolean');
+    // TextProviderBuilder
+    this._propertyDescriptors['text'] = new PrimitiveTypeProperty('text', 'setText', true, 'string');
 
-        // Selected
-        const selectedProperties = [
-            new PrimitiveTypeProperty('id', undefined, undefined, 'number'),
-            new PrimitiveTypeProperty('selected', undefined, undefined, 'boolean')
-        ];
+    // TextContainerBuilder
+    this._propertyDescriptors['textColor'] = new ArrayProperty('textColor', 'setTextColor', true, 'vec4');
+    this._propertyDescriptors['textSize'] = new PrimitiveTypeProperty('textSize', 'setTextSize', true, 'number');
 
-        this._propertyDescriptors['selected'] = new ClassProperty('selected', 'setSelected', false, selectedProperties);
+    this._propertyDescriptors['iconSize'] = new PrimitiveTypeProperty('iconSize', 'setIconSize', true, 'number');
+    this._propertyDescriptors['iconColor'] = new ArrayProperty('iconColor', 'setIconColor', true, 'vec4');
+    this._propertyDescriptors['listMaxHeight'] = new PrimitiveTypeProperty('listMaxHeight', 'setListMaxHeight', true, 'number');
+    this._propertyDescriptors['listTextSize'] = new PrimitiveTypeProperty('listTextSize', 'setListTextSize', true, 'number');
+    this._propertyDescriptors['maxCharacterLimit'] = new PrimitiveTypeProperty('maxCharacterLimit', 'setMaxCharacterLimit', true, 'number');
+    this._propertyDescriptors['multiSelect'] = new PrimitiveTypeProperty('multiSelect', 'setMultiSelect', true, 'boolean');
+    this._propertyDescriptors['showList'] = new PrimitiveTypeProperty('showList', 'showList', true, 'boolean');
+
+    // Selected
+    const selectedProperties = [
+      new PrimitiveTypeProperty('id', undefined, undefined, 'number'),
+      new PrimitiveTypeProperty('selected', undefined, undefined, 'boolean')
+    ];
+
+    this._propertyDescriptors['selected'] = new ClassProperty('selected', 'setSelected', false, selectedProperties);
+  }
+
+  create (prism, properties) {
+    this.throwIfInvalidPrism(prism);
+
+    this.validate(undefined, undefined, properties);
+
+    const element = ui.UiDropDownList.Create(prism, properties.text);
+
+    this.update(element, undefined, properties);
+
+    return element;
+  }
+
+  setSelected (element, oldProperties, newProperties) {
+    const { id, selected } = newProperties;
+
+    if (id !== undefined && selected !== undefined) {
+      element.setSelected(id, selected);
     }
+  }
 
-    create(prism, properties) {
-        this.throwIfInvalidPrism(prism);
+  validate(element, oldProperties, newProperties) {
+    super.validate(element, oldProperties, newProperties);
 
-        this.validate(undefined, undefined, properties);
-
-        const text = this._getText(properties.children);
-
-        const element = ui.UiDropDownList.Create(prism, text);
-
-        this.update(element, undefined, properties);
-
-        return element;
-    }
-
-    setSelected(element, oldProperties, newProperties) {
-        const { id, selected } = newProperties;
-
-        if ( id != undefined && selected !== undefined ) {
-            element.setSelected(id, selected);
-        }
-    }
+    PropertyDescriptor.throwIfNotTypeOf(newProperties.text, 'string');
+  }
 }
