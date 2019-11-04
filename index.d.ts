01a9e3c44972ce63971ba0a7fb9c6347c977d6b7
Tony Cuadra <tcuadra@magicleap.com>
2019-11-04 17:59:32 -0500

    Add typings for module bootstrap function

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+declare module "magic-script-components-lumin" {
+
+  import { ReactElement } from 'react';
+  import { AppProps } from 'magic-script-components'
+
+  export default class {
+    static bootstrap: (app: ReactElement<AppProps>) => void
+  }
+
+}
