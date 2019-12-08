9ac5183523d4c3e1c7d1330aff636f09b4dcdd7d
Tony Cuadra <tcuadra@magicleap.com>
2019-12-08 03:31:23 -0500

    Generate hierarchical props types

--- src/util/gen-type-script.js
+++ src/util/gen-type-script.js
@@ -1,6 +1,81 @@
 import configuration from "../configuration.js";
 import { UiNodeEvents } from '../platform/lumin-runtime/types/ui-node-events.js';
 
+export function generateTypeScript () {
+  let str = `declare module "magic-script-components" {
+
+  // Components:
+  // --------------------------------------------------------------------------------
+
+  interface AppProps {
+    type: 'landscape' | 'immersive';
+    volumeSize: vec3;
+  }
+
+`;
+
+  let deps = {};
+
+  const elements = configuration.nativeMapping.elements;
+
+  const classToProps = createClassToPropsLookup(elements);
+
+  const propsToParent = createParentLookup(elements, classToProps);
+
+  let propTypes = {};
+
+  for (let elementName in elements) {
+    const builder = elements[elementName]();
+    // capitalize the name to match exposed Component name
+    elementName = elementName[0].toUpperCase() + elementName.slice(1)
+    const name = elementName + 'Props';
+
+    const parent = propsToParent[name];
+    const list = [];
+
+    for (let propName in builder._propertyDescriptors) {
+      const propertyDescriptor = builder._propertyDescriptors[propName];
+      if (propertyDescriptor.Name !== propName) {
+        throw `Mismatched prop name, ${elementName}.${propName} !== ${propertyDescriptor.Name}`;
+      }
+      const propTs = propertyDescriptor.generateTypeScript();
+      deps = {...deps, ...propertyDescriptor.tsDependentTypes()};
+      list.push(propTs);
+    }
+
+    propTypes[name] = {
+      name,
+      parent,
+      list,
+      extraTypeScript: typeof builder.extraTypeScript === 'function'
+        ? builder.extraTypeScript() : ''
+    };
+
+    if (typeof builder.tsDependentTypes === 'function') {
+      deps = {...deps, ...builder.tsDependentTypes()};
+    }
+  }
+
+  for (const propName in propTypes) {
+    const props = propTypes[propName];
+    removeInheritedProps(props, propTypes);
+    str += generatePropsTypeScript(props);
+  }
+
+  str += genEventHandlerProps();
+
+  str += '  // Other Types:\n'
+  str += '  // --------------------------------------------------------------------------------\n\n'
+  for (let dep in deps) {
+    str += `  type ${dep} = ${deps[dep]};\n\n`;
+  }
+  str += '}\n\n';
+
+  str += 'declare function print(...args: any[]): void\n';
+
+  return str;
+}
+
 // For now just include ALL possible event handlers for all components
 // (which matches what the current implementation will allow you to register)
 function genEventHandlerProps() {
@@ -23,7 +98,6 @@ function genEventHandlerProps() {
 
 `;
 
-
   const dataTypeNames = Object.keys(eventDataTypes);
   const knownDataTypes = new Set(dataTypeNames);
   for (const dataTypeName of dataTypeNames) {
@@ -43,77 +117,89 @@ function generateEventType (dataType, knownDataTypes) {
 
   const eventData = new dataType(dummyEvent);
 
-  const parent = eventData.__proto__.__proto__;
-  const parentName = parent && parent.__proto__ ? parent.constructor.name : undefined;
-  const extendsClause = parentName ? ` extends ${parentName}` : '';
+  const parent = getParentClass(eventData);
+  const extendsClause = parent ? ` extends ${parent.name}` : '';
 
   let str = '';
-  if (parentName && !knownDataTypes.has(parentName)) {
-    knownDataTypes.add(parentName);
-    str += generateEventType(parent.constructor, knownDataTypes);
+  if (parent && !knownDataTypes.has(parent.name)) {
+    knownDataTypes.add(parent.name);
+    str += generateEventType(parent, knownDataTypes);
   }
 
   str += `  interface ${dataType.name}${extendsClause} {\n`;
   for (const key of eventData._propertyNames) {
-    str += (`    ${key}?: any;\n`);
+    str += `    ${key}?: any;\n`;
   }
   str += '  }\n\n';
   return str;
 }
 
-export function generateTypeScript () {
-  let str = `declare module "magic-script-components" {
-
-  // Components:
-  // --------------------------------------------------------------------------------
-
-  interface AppProps {
-    type: 'landscape' | 'immersive';
-    volumeSize: vec3;
+function removeInheritedProps (props, propTypes) {
+  let parentName = props.parent;
+  while (parentName && typeof propTypes[parentName] !== 'undefined') {
+    const parent = propTypes[parentName];
+    props.list = props.list.filter( propTs => !parent.list.includes(propTs) );
+    parentName = parent.parent;
   }
+}
 
-`;
+function generatePropsTypeScript (props) {
+  let comp = props.name;
+  comp = comp.substring(0, comp.length - "Props".length);
+  let str = `  interface ${props.name} extends ${props.parent ? props.parent + ', ' : ''}EventHandlerProps {\n`
+  props.list.forEach(propTs => str += `    ${propTs}\n`);
+  str += props.extraTypeScript;
+  str += '  }\n\n';
+  str += `  const ${comp}: React.FC<${props.name}>;\n\n`
+  return str;
+}
 
-  let deps = {};
+function createClassToPropsLookup (elements) {
+  let classToProps = {};
 
-  const elements = configuration.nativeMapping.elements;
   for (let elementName in elements) {
     const builder = elements[elementName]();
-    // captialize the name to match exposed Component name
-    elementName = elementName[0].toUpperCase() + elementName.slice(1)
-    str += `  interface ${elementName}Props extends EventHandlerProps {\n`
+    // capitalize the name to match exposed Component name
+    elementName = elementName[0].toUpperCase() + elementName.slice(1);
+    const propsName = elementName + 'Props';
+    classToProps[builder.constructor.name] = propsName;
+  }
 
-    for (let propName in builder._propertyDescriptors) {
-      const propertyDescriptor = builder._propertyDescriptors[propName];
-      if (propertyDescriptor.Name !== propName) {
-        throw `Mismatched prop name, ${elementName}.${propName} !== ${propertyDescriptor.Name}`;
+  // Manual intervention to allow all UiNode subclass props to extend ViewProps
+  classToProps['UiNodeBuilder'] = 'ViewProps'
+
+  return classToProps;
+}
+
+function createParentLookup (elements, classToProps) {
+  let propsToParent = {};
+    for (let elementName in elements) {
+    const builder = elements[elementName]();
+    // capitalize the name to match exposed Component name
+    elementName = elementName[0].toUpperCase() + elementName.slice(1);
+    const propsName = elementName + 'Props';
+    classToProps[builder.constructor.name] = propsName;
+
+    let parentBuilderClass = getParentClass(builder);
+    while (parentBuilderClass) {
+      if (typeof classToProps[parentBuilderClass.name] !== 'undefined') {
+        const parentPropsName = classToProps[parentBuilderClass.name];
+        if (parentPropsName !== propsName) {
+          propsToParent[propsName] = parentPropsName;
+          break;
+        }
       }
-      const propTs = propertyDescriptor.generateTypeScript();
-      deps = {...deps, ...propertyDescriptor.tsDependentTypes()};
-      str += `    ${propTs}\n`;
+      const parentBuilder = new parentBuilderClass();
+      parentBuilderClass = getParentClass(parentBuilder);
     }
-
-    if (typeof builder.extraTypeScript === 'function') {
-      str += builder.extraTypeScript();
-    }
-    if (typeof builder.tsDependentTypes === 'function') {
-      deps = {...deps, ...builder.tsDependentTypes()};
-    }
-
-    str += '  }\n\n';
-    str += `  const ${elementName}: React.FC<${elementName}Props>;\n\n`
   }
+  // ListViewProps has itemAlignement which conflicts with Content -- remove parent as workaround
+  propsToParent['ListViewProps'] = undefined;
 
-  str += genEventHandlerProps();
+  return propsToParent;
+}
 
-  str += '  // Other Types:\n'
-  str += '  // --------------------------------------------------------------------------------\n\n'
-  for (let dep in deps) {
-    str += `  type ${dep} = ${deps[dep]};\n\n`;
-  }
-  str += '}\n\n';
-
-  str += 'declare function print(...args: any[]): void\n';
-
-  return str;
+function getParentClass (obj) {
+  const parent = obj.__proto__.__proto__;
+  return parent && parent.__proto__ ? parent.constructor : undefined;
 }
