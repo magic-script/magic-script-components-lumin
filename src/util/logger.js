6af28eeefec09573ec6c3c257b224a770a197854
grozdanov <nikolay.grozdanov@gmail.com>
2020-01-22 00:01:30 -0500

    Expand loggin options

--- src/util/logger.js
+++ src/util/logger.js
@@ -10,15 +10,34 @@ Object.defineProperties(MessageSeverity, {
   'exception': { enumerable: true, writable: false, value: 3 }
 });
 
-export { MessageSeverity };
+function logWithSeverity(severity, ...messageArgs) {
+  if (severity >= configuration.logMinMessageSeverity) {
+    console.log(...messageArgs);
+  }
+}
+
+function logInfo(...messageArgs) {
+  logWithSeverity(MessageSeverity.info, ...messageArgs);
+}
+
+function logWarning(...messageArgs) {
+  logWithSeverity(MessageSeverity.warning, ...messageArgs);
+}
+
+function logError(...messageArgs) {
+  logWithSeverity(MessageSeverity.error, ...messageArgs);
+}
+
+function logException(...messageArgs) {
+  logWithSeverity(MessageSeverity.exception, ...messageArgs);
+}
+
+export { MessageSeverity, logWithSeverity, logInfo, logWarning, logError, logException };
 
 // Logger definition
 function log (message, messageSeverity) {
-    const severity = messageSeverity === undefined ? MessageSeverity.info : messageSeverity;
-
-    if (severity >= configuration.logMinMessageSeverity) {
-      console.log(message);
-    }
-  }
+  const severity = messageSeverity === undefined ? MessageSeverity.info : messageSeverity;
+  logWithSeverity(severity, message)
+}
 
 export default log;
